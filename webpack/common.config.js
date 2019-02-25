30cca9c561f10f96ed5cafc6df62c4562032a69d
matsunagakenta <k_matsunaga@waku-2.com>
2019-02-25 17:57:44 +0900

    fit entry of webpack

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index',
   output: {
     path: path.resolve(__dirname, '../build'),
     filename: 'calendar.js'
